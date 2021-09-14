d61220713ccf1df92b78145f4c80c0844a8d9424
cDillanb <85910223+cDillanb@users.noreply.github.com>
2021-09-14 12:01:44 -0600

    added rollbar functionality

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const Rollbar = require("rollbar");
+
+let rollbar = new Rollbar({
+    accessToken: "e79761c25fa74dd7993f1823abe8a8da",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
 
 const app = express();
 app.use(express.json());
 app.use("/style", express.static("./public/styles.css"));
 
-const port = process.env.PORT || 5050;
-
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-})
+});
+
+app.get("/api/picture", (req, res) => {
+
+});
+
+const port = process.env.PORT || 5050;
+
+app.use(rollbar.errorHandler());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
